96cb0421dcff6b2a555432c386132a45f3aaab74
Ortiz10101 <ortizlj10101@gmail.com>
2023-04-29 01:06:51 -0600

    cambios

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,56 +1,107 @@
-import logger from '../../lib/logger';
-import IProduct from '../interfaces/product.interface';
 import IResponse from '../interfaces/response.interface';
-import Product from '../models/product.model'
-
+import productSchema from '../models/product.model'
+import IProduct from '../interfaces/product.interface';
+import logger from '../../lib/logger';
 
 export default class ProductController {
-    registerProduct(product: IProduct): Promise<IResponse> {
+
+    createProduct(product: IProduct): Promise<IResponse> {
         logger.info(`creating ${product}`)
         return new Promise ((resolve, reject) => {
             if (!product) {
-                return reject ({ ok: false, message: 'datos incorrectos', response: null, code:400 })
+                return reject ({ ok: false, message: 'Parametros incorrectos', response: null, code:400 })
             }
-            Product.create(product).then((res: any) => {
-                logger.info(`${product} creado exitosamente`)
-                return resolve({ ok: true, message: 'producto creado con exito', response: res, code: 201})
-                }).catch((err: any) => {
-                logger.error(err)
-                return reject({ ok: false, message: 'Error del servidor', response: err, code:500 })
-            })
+            productSchema.create(product).then((res: any) => {
+                    logger.info(`${product}fue agregado correctamente`)
+                    return resolve ({ ok: true, message: 'producto agregado correctamente', response: res, code:201 })
+                }).catch((err: any) =>{
+                    logger.error(err)
+                    return reject({ ok: false, message: 'Error del servidor', response: err, code:500 })
+            }
+
+        )}
+    )}
+    
+    readProductByCategory(category: string): Promise<IResponse> {
+        const regexp = new RegExp(category, 'i')
+        return new Promise(async(resolve, reject) =>{
+            if (!category){
+                    return reject({ok: false, message: 'parametros incorrectos', response: null, code: 500 })
+                }
+                try{
+                    const result: any = await productSchema.find({ category: regexp}) 
+                    if (result.length < 1){
+                        return reject({ok: false, message: 'producto no encontrado', response: null, code: 404})
+                    }
+                    return resolve({ ok: true, message: 'Producto encontrado', response: result, code: 200 })
+                }catch(e){
+                    return reject({ ok: false, message: 'Error del servidor', response: e, code:500 })
+
+                }
         })
     }
+    readProductByName(name: string): Promise<IResponse> {
+        const regexp = new RegExp(name, 'i')
+        return new Promise(async(resolve, reject) =>{
+            if (!name){
+                    return reject({ok: false, message: 'parametros incorrectos', response: null, code: 500 })
+                }
+                try{
+                    const result: any = await productSchema.find({ name: regexp}) 
+                    if (result.length < 1){
+                        return reject({ok: false, message: 'producto no encontrado', response: null, code: 404})
+                    }
+                    return resolve({ ok: true, message: 'Producto encontrado', response: result, code: 200 })
+                }catch(e){
+                    return reject({ ok: false, message: 'Error del servidor', response: e, code:500 })
 
-    getAllForRegExp(value: string): Promise<IResponse> {
-        const regexp = new RegExp(value, 'i');
-
-        return new Promise(async(resolve, reject) => {
+                }
+        })
+    }
+    
+    populateProducts(): Promise<boolean> {
+        return new Promise(async (resolve, reject) => {
             try {
-                const result: any = await Product.find({ name: regexp })
-                if ( result.length < 1 ) {
-                    return reject({ok: false, message: 'producto no encontrado', response: null, code: 404})
+                const products = [
+                    {
+                        name: 'Samsung Galaxy',
+                        category: 'electronics'
+                    },
+                    {
+                        name: 'Motorola V3',
+                        category: 'electronics'
+                    },
+                    {
+                        name: 'Iphone 12',
+                        category: 'electronics'
+                    },
+                    {
+                        name: 'Skippy',
+                        category: 'grocery_store'
+                    }
+                ]
+
+                const data = await productSchema.find({})
+
+                if (data && data.length < 1) {
+                    for( let product of products) {
+                        try {
+                            await this.createProduct(product)
+                            logger.info(`Product created ${product}`)
+                        } catch(e) {
+                            logger.error(e)
+                            return reject(false)
+                        }
+                    }
                 }
 
-                return resolve({ ok: true, message: 'Producto encontrado', response: result, code: 200 })
-            }catch(e) {
-                return reject({ ok: false, message: 'Error del servidor', response: e, code:500 })
+                return resolve(true)
+
+
+            } catch(e) {
+                logger.error(e)
+                return reject(false)
             }
         })
     }
-
-    surgeryProduct(category: string): Promise<IResponse> {
-        return new Promise(async(resolve, reject) => {
-            try {
-                const result: any = await Product.find({ category: category })
-                if ( result.length < 1 ) {
-                    return reject({ok: false, message: 'producto no encontrado', response: null, code: 404})
-                }
-                const suggestion = result.slice(0, 2)
-                
-                return resolve({ ok: true, message: 'Producto encontrado', response: suggestion, code: 200 })
-            }catch(e) {
-                return reject({ ok: false, message: 'Error del servidor', response: e, code:500 })
-            }
-        })
-    }  
 }
--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -4,6 +4,6 @@ import mongoose,  { Schema } from "mongoose";
 const productSchema: Schema = new Schema ({
     name: { type: String, required: true },
     category: { type: String, required: true, enum: ['electronic', 'grocery_store'] }
-}, { collection: 'Products' })
+}, { collection: 'products' })
 
 export default mongoose.model('Product', productSchema)
